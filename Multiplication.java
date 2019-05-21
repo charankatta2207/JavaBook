51ca67296a5c53dddcc8284b165a12b7875052b2
charankatta2207 <cha6nkatta@gmail.com>
2019-05-21 18:18:26 +0530

    add Multiplication file

---
+++ Multiplication.java
@@ -0,0 +1,13 @@
+public class Multiplication
+{
+public static void main(String args[])
+{
+int i;
+Scanner sc = new Scanner(System.in);
+System.out.println("Enter any no.:");
+int n = sc.nextInt();
+for(i=0;i<=10;i++)
+{
+System.out.println(n+"*"+i+"="+n*i);
+}
+}
