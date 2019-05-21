45cc0f9fc9c9b799699046a5079ce2f4c8f26961
charankatta2207 <cha6nkatta@gmail.com>
2019-05-21 18:33:50 +0530

    add Multiplication file

---
+++ Multiplication1.java
@@ -0,0 +1,15 @@
+public class Multiplication1
+{
+public static void main(String args[])
+{
+int i,j;
+for(j=1;j<=20;j++)
+{
+for(i=1;i<=10;i++)
+{
+System.out.println(j+"*"+i+"="+j*i);
+}
+}
+}
+}
+
