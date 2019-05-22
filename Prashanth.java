7a7a701e3cf5e7b4b00cc971a293ff4155d4626b
charankatta2207 <cha6nkatta@gmail.com>
2019-05-22 14:20:42 +0530

    add Prashanth file

---
+++ Prashanth.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class prashanth
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter your name:");
+String p = sc.nextLine();
+System.out.println("hello,"+p );
+}
+}
+
